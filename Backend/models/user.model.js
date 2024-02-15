7e0241f98bf443c52611324b83538b0c3b37a924
asifpatel068 <asifpatel068@gmail.com>
2024-02-15 11:38:49 +0530

    commited

--- Backend/models/user.model.js
+++ Backend/models/user.model.js
@@ -1,15 +1,29 @@
 const mongoose=require("mongoose")
 
-const userScheema = mongoose.Schema({
-    name:String,
-    email:String,
-    phone:String,
-    website:String,
-    city:String,
-    company:String
-})
+const userSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    username: { type: String, required: true },
+    email: { type: String, required: true },
+    address: {
+      street: String,
+      suite: String,
+      city: String,
+      zipcode: String,
+      geo: {
+        lat: String,
+        lng: String
+      }
+    },
+    phone: String,
+    website: String,
+    company: {
+      name: String,
+      catchPhrase: String,
+      bs: String
+    }
+  });
 
-const User=mongoose.model("user",userScheema)
+const User=mongoose.model("user",userSchema)
 
 module.exports={
     User
